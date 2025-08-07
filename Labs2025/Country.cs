c28e880ddef75ada772ea4be551058616423e215
Maksym Oleksiienko <starsredmaks@gmail.com>
2025-08-07 20:24:20 +0300

    Add solution

---
+++ Labs2025/Country.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs2025
+{
+    internal class Country
+    {
+        string _name;
+        int _square;
+        int _population;
+        string _capitalName;
+        int _capitalPopulation;
+
+        public Country(string name, int square, int population, string capitalName, int capitalPopulation)
+        {
+            _name = name;
+            _square = square;
+            _population = population;
+            _capitalName = capitalName;
+            _capitalPopulation = capitalPopulation;
+        }
+
+        public string GetString()
+        {
+            return $"Country: {_name}\n" +
+                $"Square(km^2): {_square}\n" +
+                $"Population: {_population}\n" +
+                $"Capital name: {_capitalName}\n" +
+                $"Capital population: {_capitalPopulation}";
+        }
+    }
+}
--- Labs2025/Form1.cs
+++ Labs2025/Form1.cs
@@ -16,5 +16,14 @@ namespace Labs2025
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Country first = new Country("United Kingdom", 244_376, 68_265_209, "London", 8_945_309);
+            Country second = new Country("United States", 8_080_470, 340_110_988, "Washington, D.C.", 689_545);
+            MessageBox.Show(first.GetString());
+            MessageBox.Show(second.GetString());
+            Close();
+        }
     }
 }
